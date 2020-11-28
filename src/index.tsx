e24a6df658a1ab253889cd7a24981d2f6938ad0f
Patrick Odenborg <patrick@odenborg.com>
2020-11-27 19:06:47 -0600

    Remove unused comments

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,6 @@ import { DndProvider } from "react-dnd"
 import { HTML5Backend as Backend } from "react-dnd-html5-backend"
 import { AppStateProvider } from "./AppStateContext"
 
-/* BOOKMARK: PAGE 139 */
-/* NEXT SECTION => 'How to Test Your Applications' */
-
 ReactDOM.render(
   <React.StrictMode>
     <DndProvider backend={Backend}>
